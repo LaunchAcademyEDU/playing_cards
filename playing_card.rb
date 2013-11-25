ca222bfc99819221c40b64ef9b25fafabc53636a
Dan Pickett <dan.pickett@launchware.com>
2013-11-25 07:52:42 -0500

    add deck and player hand classes

--- playing_card.rb
+++ playing_card.rb
@@ -1,3 +1,23 @@
+
+
+# seven_spades = PlayingCard.new('7', 'spades')
+# four_diamonds = PlayingCard.new('4', 'diamonds')
+# king_hearts = PlayingCard.new('K', 'hearts')
+
+# hand = [seven_spades, four_diamonds, king_hearts]
+
+# hand.each do |card|
+#   if card.face_card?
+#     puts "Has a face card!"
+#   else
+#     puts "Nope...not a face card"
+#   end
+# end
+
+
+SUITS = ['♠', '♣', '♥', '♦']
+VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
 class PlayingCard
   def initialize(rank, suit)
     @rank = rank
@@ -7,23 +27,61 @@ class PlayingCard
   def face_card?
     ['J', 'Q', 'K'].include?(@rank)
   end
-end
 
-seven_spades = PlayingCard.new('7', 'spades')
-four_diamonds = PlayingCard.new('4', 'diamonds')
-king_hearts = PlayingCard.new('K', 'hearts')
+  def rank
+    @rank
+  end
 
-hand = [seven_spades, four_diamonds, king_hearts]
-
-hand.each do |card|
-  if card.face_card?
-    puts "Has a face card!"
-  else
-    puts "Nope...not a face card"
+  def suit
+    @suit
   end
 end
 
+class Deck
+  def initialize
+    @collection = []
+    SUITS.each do |suit|
+      VALUES.each do |value|
+        @collection << PlayingCard.new(value, suit)
+      end
+    end
 
-SUITS = ['♠', '♣', '♥', '♦']
-VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    @collection.shuffle!
+  end
+
+  def draw!
+    @collection.pop
+  end
+end
+
+class PlayerHand
+  def initialize(collection)
+    @collection = collection
+  end
+
+  def score
+    score = 0
+    @collection.each do |card|
+      puts card.rank
+      if card.face_card?
+        score += 15
+      elsif card.rank == 'A'
+        score += 1
+      else
+        score += card.rank.to_i
+      end
+    end
+
+    score
+  end
+end
+
+deck = Deck.new
+
+card_1 = deck.draw!
+card_2 = deck.draw!
+
+hand = PlayerHand.new([card_1, card_2])
+puts hand.score
+
 
